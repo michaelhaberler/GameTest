a4b6781412e79e601c38f9cf48e2874c231b9da9
Michael Haberler <michael.haberler@gmx.at>
2014-03-25 14:31:13 +0100

    god's random walks - offensichtlich würfelt er doch

--- app/src/main/java/at/appitizer/gametest/app/GameView.java
+++ app/src/main/java/at/appitizer/gametest/app/GameView.java
@@ -5,20 +5,29 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.util.LinkedList;
 
 /**
  * Created by michael on 25/03/14.
  */
 public class GameView extends SurfaceView {
     private static final String TAG = "GameView";
+    private static final int TRAJECTORY_SIZE = 10000;
     private final Bitmap bmp;
     private SurfaceHolder holder;
     private GameLoopThread gameLoopThread;
-    private int x = 0;
-    private int y = 0;
+    //private int x = 0;
+    //private int y = 0;
+    private Point currPosition = new Point(0, 0);
+    private Paint trajectoryPaint;
+    private LinkedList<Point> trajectory = new LinkedList<Point>();
 
     public GameView(Context context) {
         super(context);
@@ -42,8 +51,8 @@ public class GameView extends SurfaceView {
 
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-                x = getWidth() / 2;
-                y = getHeight() / 2;
+                currPosition.x = getWidth() / 2;
+                currPosition.y = getHeight() / 2;
                 gameLoopThread.setRunning(true);
                 gameLoopThread.start();
             }
@@ -53,6 +62,11 @@ public class GameView extends SurfaceView {
                                        int width, int height) {
             }
         });
+
+        trajectoryPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        trajectoryPaint.setStyle(Paint.Style.STROKE);
+        trajectoryPaint.setColor(Color.WHITE);
+
         bmp = BitmapFactory.decodeResource(getResources(), R.drawable.god1);
     }
 
@@ -61,10 +75,25 @@ public class GameView extends SurfaceView {
         canvas.drawColor(Color.BLACK);
         int dx = (int) Math.signum(Math.random() - 0.5);
         int dy = (int) Math.signum(Math.random() - 0.5);
-        if (x < getWidth() - bmp.getWidth() && y < getHeight() - bmp.getHeight()) {
-            x += dx;
-            y += dy;
+        if (currPosition.x < getWidth() - bmp.getWidth() && currPosition.y < getHeight() - bmp.getHeight()) {
+            currPosition.x += dx;
+            currPosition.y += dy;
+
+            if (trajectory.size() > TRAJECTORY_SIZE) {
+                trajectory.removeFirst();
+            }
+            trajectory.addLast(new Point(currPosition.x, currPosition.y));
         }
-        canvas.drawBitmap(bmp, x, y, null);
+        Path trajectoryPath = new Path();
+        Point start = trajectory.getFirst();
+        trajectoryPath.moveTo(start.x, start.y);
+        for (Point p: trajectory) {
+            trajectoryPath .lineTo(p.x, p.y);
+        }
+        canvas.drawPath(trajectoryPath, trajectoryPaint);
+
+        float left = currPosition.x - bmp.getWidth() / 2;
+        float right = currPosition.y - bmp.getHeight() / 2;
+        canvas.drawBitmap(bmp, left, right, null);
     }
 }
