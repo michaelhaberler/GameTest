26689e74e36b3d069f616354497362e4ffb7c9a1
Michael Haberler <michael.haberler@gmx.at>
2014-03-25 15:49:16 +0100

    bad guy walks around randomly always looking in front

--- app/src/main/java/at/appitizer/gametest/app/GameView.java
+++ app/src/main/java/at/appitizer/gametest/app/GameView.java
@@ -5,29 +5,17 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Path;
-import android.graphics.Point;
-import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-
-import java.util.LinkedList;
 
 /**
  * Created by michael on 25/03/14.
  */
 public class GameView extends SurfaceView {
     private static final String TAG = "GameView";
-    private static final int TRAJECTORY_SIZE = 10000;
-    private final Bitmap bmp;
+    private final Sprite sprite;
     private SurfaceHolder holder;
     private GameLoopThread gameLoopThread;
-    //private int x = 0;
-    //private int y = 0;
-    private Point currPosition = new Point(0, 0);
-    private Paint trajectoryPaint;
-    private LinkedList<Point> trajectory = new LinkedList<Point>();
 
     public GameView(Context context) {
         super(context);
@@ -43,16 +31,13 @@ public class GameView extends SurfaceView {
                     try {
                         gameLoopThread.join();
                         retry = false;
-                    }
-                    catch (InterruptedException e) {
+                    } catch (InterruptedException e) {
                     }
                 }
             }
 
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-                currPosition.x = getWidth() / 2;
-                currPosition.y = getHeight() / 2;
                 gameLoopThread.setRunning(true);
                 gameLoopThread.start();
             }
@@ -63,38 +48,13 @@ public class GameView extends SurfaceView {
             }
         });
 
-        trajectoryPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        trajectoryPaint.setStyle(Paint.Style.STROKE);
-        trajectoryPaint.setColor(Color.WHITE);
-
-        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.god1);
+        Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.bad1);
+        sprite = new Sprite(this, bmp);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawColor(Color.BLACK);
-        int dx = (int) Math.signum(Math.random() - 0.5);
-        int dy = (int) Math.signum(Math.random() - 0.5);
-
-        // the god may walk out of sight
-        currPosition.x += dx;
-        currPosition.y += dy;
-
-        if (trajectory.size() > TRAJECTORY_SIZE) {
-            trajectory.removeFirst();
-        }
-        trajectory.addLast(new Point(currPosition.x, currPosition.y));
-
-        Path trajectoryPath = new Path();
-        Point start = trajectory.getFirst();
-        trajectoryPath.moveTo(start.x, start.y);
-        for (Point p: trajectory) {
-            trajectoryPath .lineTo(p.x, p.y);
-        }
-        canvas.drawPath(trajectoryPath, trajectoryPaint);
-
-        float left = currPosition.x - bmp.getWidth() / 2;
-        float right = currPosition.y - bmp.getHeight() / 2;
-        canvas.drawBitmap(bmp, left, right, null);
+        sprite.onDraw(canvas);
     }
 }
---
+++ app/src/main/java/at/appitizer/gametest/app/Sprite.java
@@ -0,0 +1,114 @@
+package at.appitizer.gametest.app;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.util.Log;
+
+import java.util.LinkedList;
+
+/**
+ * Created by michael on 25/03/14.
+ *
+ * spritesheet
+ * 0 1 2 frames
+ * 0 down animation
+ * 1 left
+ * 2 right
+ * 3 up
+ *
+ * currSpeed to spritesheet mapping
+ * (http://www.edu4java.com/en/androidgame/androidgame5.html)
+ * 	x	 y	 atan2(x,y)	 atan2(x,y)/(PI/2)	 (atan2(x,y)/(PI/2)+2)%4	 bmp row (from 0)
+ * 	up	 0	 -1	 PI or -PI	 2 or -2	 4 or 0	 3
+ * 	right	 1	 0	 PI/2	 1	 3	 2
+ * 	down	 0	 1	 0	 0	 2	 0
+ * 	left	 -1	 0	 -PI/2	 -1	 1	 1
+ */
+public class Sprite {
+
+    private static final String TAG = "Sprite";
+    private GameView gameView;
+    private Bitmap bmp;
+
+    private static final int BMP_ROWS = 4;
+    private static final int BMP_COLUMNS = 3;
+    private int currFrame = 0;
+    private int width;
+    private int height;
+
+    private Point currPosition = new Point(0, 0);
+    private Point currSpeed = new Point(1, 1);
+    private Paint trajectoryPaint;
+    {
+        trajectoryPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        trajectoryPaint.setStyle(Paint.Style.STROKE);
+        trajectoryPaint.setColor(Color.WHITE);
+    }
+    private static final int TRAJECTORY_POSITION_SIZE = 10000;
+    private LinkedList<Point> trajectoryPosition = new LinkedList<Point>();
+
+    private int currTime = 0;
+
+    public Sprite(GameView gameView, Bitmap bmp) {
+        this.gameView=gameView;
+        this.bmp=bmp;
+        currPosition.x = this.bmp.getWidth();
+        currPosition.y = this.bmp.getHeight();
+
+        this.width = bmp.getWidth() / BMP_COLUMNS;
+        this.height = bmp.getHeight() / BMP_ROWS;
+    }
+
+    private void update() {
+        currTime++;
+        if (currTime % 30 == 0) {
+            currSpeed.x = (int) Math.round(Math.random()*2 - 1);
+            currSpeed.y = (int) Math.round(Math.random()*2 - 1);
+        }
+
+        currPosition.x += currSpeed.x;
+        currPosition.y += currSpeed.y;
+        currFrame = ++currFrame % BMP_COLUMNS;
+
+        if (trajectoryPosition.size() > TRAJECTORY_POSITION_SIZE) {
+            trajectoryPosition.removeFirst();
+        }
+        trajectoryPosition.addLast(new Point(currPosition.x, currPosition.y));
+
+        //Log.d(TAG, "\ncurrPosition = " + currPosition.x + ", " + currPosition.y + ", " +
+        //        "\ncurrSpeed = " + currSpeed.x + ", " + currSpeed.y);
+    }
+
+    public void onDraw(Canvas canvas) {
+        update();
+
+        Path trajectoryPath = new Path();
+        Point start = trajectoryPosition.getFirst();
+        trajectoryPath.moveTo(start.x, start.y);
+        for (Point p: trajectoryPosition) {
+            trajectoryPath .lineTo(p.x, p.y);
+        }
+        canvas.drawPath(trajectoryPath, trajectoryPaint);
+
+        int srcX = currFrame * width;
+        int srcY = getAnimationRow() * height;
+        Rect src = new Rect(srcX, srcY, srcX + width, srcY + height);
+        Rect dst = new Rect(currPosition.x-width/2, currPosition.y-height/2, currPosition.x + width, currPosition.y + height);
+        canvas.drawBitmap(bmp, src, dst, null);
+    }
+
+    // direction = 0 up, 1 left, 2 down, 3 right,
+    // animation = 3 up, 1 left, 0 down, 2 right
+    int[] DIRECTION_TO_ANIMATION_MAP = { 3, 1, 0, 2 };
+
+    private int getAnimationRow() {
+        double dirDouble = (Math.atan2(currSpeed.x, currSpeed.y) / (Math.PI / 2) + 2);
+        int direction = (int) Math.round(dirDouble) % BMP_ROWS;
+        return DIRECTION_TO_ANIMATION_MAP[direction];
+    }
+}
